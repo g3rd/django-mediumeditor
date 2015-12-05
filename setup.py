932d686eb326ef1fb3b7e3aa9dfd94fb5f140149
g3rd <chad@aceportfol.io>
2015-12-04 21:45:35 -0500

    Made installable

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+long_description = open('README.md').read()
+
+setup(
+    name="django-mediumeditor",
+    version='0.1',
+    packages=["mediumeditor"],
+    include_package_data=True,
+    description="Medium Editor widget for Django",
+    url="https://github.com/g3rd/django-mediumeditor",
+    author="Chad Shryock",
+    author_email="chad@aceportfol.io",
+    license='MIT',
+    long_description=long_description,
+    platforms=["any"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
